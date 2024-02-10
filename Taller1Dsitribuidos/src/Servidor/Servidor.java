e018e6e6583fc7654cdce758f4831cc88b92b9c7
Laura Ovalle Benítez <ovalle.laura@javeriana.edu.co>
2024-02-10 12:31:30 -0500

    Update Servidor.java
    Base del proyecto ready

--- Taller1Dsitribuidos/src/Servidor/Servidor.java
+++ Taller1Dsitribuidos/src/Servidor/Servidor.java
@@ -4,21 +4,12 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.Socket;
 
 import Conexion.Conexion;
 
 public class Servidor extends Conexion //Se hereda de conexión para hacer uso de los sockets y demás
 {
-	private Socket servidor1, servidor2; // Sockets para los dos servidores adicionales
-	
-	
-    public Servidor() throws IOException{
-    	super("servidor","192.168.56.1");
-    	servidor1 = new Socket("192.168.1.100", 1234); // Dirección y puerto del servidor 1
-    	servidor2 = new Socket("192.168.1.101", 1234); // Dirección y puerto del servidor 2 //Se usa el constructor para servidor de Conexion
-    }
-   
+    public Servidor() throws IOException{super("servidor",null);} //Se usa el constructor para servidor de Conexion
 
     public void startServer()//Método para iniciar el servidor
     {
@@ -44,13 +35,6 @@ public class Servidor extends Conexion //Se hereda de conexión para hacer uso de
                 //Se muestra por pantalla el mensaje recibido
                 System.out.println(mensajeServidor);
                 //Mandar petición a los servidores bebé
-                if (mensajeServidor.equals("accion1")) {
-                    enviarMensaje(servidor1, "Mensaje para servidor 1");
-                } else if (mensajeServidor.equals("accion2")) {
-                    enviarMensaje(servidor2, "Mensaje para servidor 2");
-                } else {
-                    System.out.println("Mensaje no reconocido");
-                }
             }
 
             System.out.println("Fin de la conexión");
@@ -61,10 +45,5 @@ public class Servidor extends Conexion //Se hereda de conexión para hacer uso de
         {
             System.out.println(e.getMessage());
         }
-    }
-    
-    private void enviarMensaje(Socket servidor, String mensaje) throws IOException {
-        DataOutputStream salidaServidor = new DataOutputStream(servidor.getOutputStream());
-        salidaServidor.writeUTF(mensaje);
     }
 }
